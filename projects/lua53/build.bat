ca3218f4899e745feabc4e608277bccb5308efee
uniqss <uniqs@163.com>
2021-10-26 20:06:32 +0800

    lua53

---
+++ projects/lua53/build.bat
@@ -0,0 +1,26 @@
+
+rm -rf build
+mkdir build
+cd build
+
+set BAT_BUILD_TYPE=relwithdebinfo
+
+
+cmake -A x64 -DCMAKE_BUILD_TYPE=%BAT_BUILD_TYPE% ..
+cmake --build . --config %BAT_BUILD_TYPE%
+
+
+cd ..
+
+set TMP_DEST_PATH=..\include\lua\
+set TMP_SRC_PATH=.\src\
+cp -R %TMP_SRC_PATH%lua.hpp %TMP_DEST_PATH%
+cp -R %TMP_SRC_PATH%lua.h %TMP_DEST_PATH%
+cp -R %TMP_SRC_PATH%luaconf.h %TMP_DEST_PATH%
+cp -R %TMP_SRC_PATH%lualib.h %TMP_DEST_PATH%
+cp -R %TMP_SRC_PATH%lauxlib.h %TMP_DEST_PATH%
+
+xcopy /Y /S /Q .\bin\%BAT_BUILD_TYPE%\*.lib ..\lib\
+
+
+@REM pause
---
+++ projects/lua53/clean_all.bat
@@ -0,0 +1,5 @@
+rm -rf build
+rm -rf bin
+
+set TMP_DEST_PATH=..\include\lua\
+rm -rf %TMP_DEST_PATH%
